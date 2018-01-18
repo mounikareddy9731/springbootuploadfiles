e4dd90ba5cc6bdf2eb50a28b516af56c2eef3ebe
Mounika Nanabolu <mounika9731@gmail.com>
2018-01-18 11:59:04 -0500

    First Commit OK

--- src/main/java/com/hellokoding/uploadingfiles/Application.java
+++ src/main/java/com/hellokoding/uploadingfiles/Application.java
@@ -11,5 +11,6 @@ public class Application {
     public static void main(String[] args) throws IOException {
         new File(UploadingController.uploadingdir).mkdirs();
         SpringApplication.run(Application.class, args);
-    }
+    }//Sprinbot
+   // application
 }
